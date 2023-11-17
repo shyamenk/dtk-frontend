a5916c0aaa3bd47a63cd888f69d99998a6eb0490
shyam <shyamenk@gmail.com>
2023-11-18 00:20:27 +0530

    TAILWIND CSS ADDED

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>Doctors Time Keep V1</h1>
+      <h1 className="text-5xl text-red-500">Doctors Time Keep V1</h1>
     </div>
   );
 }
